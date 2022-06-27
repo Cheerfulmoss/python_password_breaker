6163d36cf9d03e0f35a9804e5fc36717ebe93147
Cheerfulmoss <41566305+Cheerfulmoss@users.noreply.github.com>
2022-06-27 11:15:56 +1000

    Update bruteforce_algorithms.py

--- bruteforce_algorithms.py
+++ bruteforce_algorithms.py
@@ -1,7 +1,7 @@
-import itertools
-import string
-import time
-import threading
+from itertools import product
+from string import printable
+from time import time
+from threading import Thread
 
 
 class PasswordBreaker:
@@ -10,15 +10,15 @@ class PasswordBreaker:
         self.guessed_password = ""
         self.attempts = 0
         self.stop_threads = False
+        self.characters = printable
 
     def guess_password(self, password, lengths_list):
-        characters = string.printable
         while self.stop_threads is False:
             if type(lengths_list) == list:
                 running = True
                 while running:
                     for length in lengths_list:
-                        for guess in itertools.product(characters, repeat=length):
+                        for guess in product(self.characters, repeat=length):
                             if self.stop_threads:
                                 break
                             self.attempts += 1
@@ -33,7 +33,7 @@ class PasswordBreaker:
                             break
                     running = False
             else:
-                for guess in itertools.product(characters, repeat=lengths_list):
+                for guess in product(self.characters, repeat=lengths_list):
                     if self.stop_threads:
                         break
                     self.attempts += 1
@@ -46,7 +46,7 @@ class PasswordBreaker:
 
     def start_threads(self, password, lengths_list, max_threads):
         for letter in password:
-            if letter not in string.printable:
+            if letter not in self.characters:
                 raise ValueError(f"Invalid characters")
         for length in lengths_list:
             if length < 1:
@@ -56,22 +56,16 @@ class PasswordBreaker:
         if len(lengths_list) > max_threads:
             split_lengths_list = [lengths_list[i::max_threads] for i in range(max_threads)]
             for index in range(max_threads):
-                t = threading.Thread(target=self.guess_password, args=(password, split_lengths_list[index]))
+                t = Thread(target=self.guess_password, args=(password, split_lengths_list[index]))
                 threads.append(t)
         else:
             for length in lengths_list:
-                t = threading.Thread(target=self.guess_password, args=(password, length))
+                t = Thread(target=self.guess_password, args=(password, length))
                 threads.append(t)
-        start_time = time.time()
+        start_time = time()
         for thread in threads:
             thread.start()
         for finished_thread in threads:
             finished_thread.join()
-        total_time = time.time() - start_time
+        total_time = time() - start_time
         return self.guessed_password, self.attempts, total_time
-
-
-password = input(": ")
-guess = PasswordBreaker().start_threads(password, [3, 4, 5], 5)
-
-print(f"Password was {guess[0]}, {guess[1]} attempts took {guess[2]} seconds")
